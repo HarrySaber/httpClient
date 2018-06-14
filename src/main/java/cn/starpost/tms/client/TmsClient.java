78a0f2971952cf416785f5437a9d3c0eb6538744
dongjianpeng <541874767@qq.com>
2018-06-14 12:57:23 +0800

    charge

--- src/main/java/cn/starpost/tms/client/TmsClient.java
+++ src/main/java/cn/starpost/tms/client/TmsClient.java
@@ -29,14 +29,16 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import cn.starpost.tms.client.utils.HttpClientUtil;
 import cn.starpost.tms.client.value.channel.FindChannelRequest;
 import cn.starpost.tms.client.value.channel.FindChannelResponse;
+import cn.starpost.tms.client.value.charge.FindChargeByOrderIdsRequest;
+import cn.starpost.tms.client.value.charge.FindChargeByOrderIdsResponse;
 import cn.starpost.tms.client.value.pdf.GetCartonLabelPdfRequest;
 import cn.starpost.tms.client.value.pdf.GetCartonLabelPdfResponse;
 import cn.starpost.tms.client.value.provider.FindServiceProvideRequest;
 import cn.starpost.tms.client.value.provider.FindServiceProvideResponse;
 import cn.starpost.tms.client.value.warehouse.FindAllFbaWarehouseRequest;
 import cn.starpost.tms.client.value.warehouse.FindAllFbaWarehouseResponse;
-import cn.starpost.tms.client.value.warehouse.FindOrderPrefixRequest;
 import cn.starpost.tms.client.value.warehouse.FindOrderIdResponse;
+import cn.starpost.tms.client.value.warehouse.FindOrderPrefixRequest;
 import cn.starpost.tms.client.value.warehouse.FindWarehouseAddressByWarehouseCodeRequest;
 import cn.starpost.tms.client.value.warehouse.FindWarehouseAddressByWarehouseCodeResponse;
 import cn.starpost.tms.client.value.warehouse.FindWarehouseAddressRequest;
@@ -250,6 +252,24 @@ public class TmsClient {
 		} catch (Exception e) {
 			return GetCartonLabelPdfResponse.failed(e.getLocalizedMessage());
 
+		}
+	}
+	
+	public FindChargeByOrderIdsResponse findChargeByOrderIds(FindChargeByOrderIdsRequest request) {
+		try {
+			String url = baseUrl + "/api/charge";
+			ObjectMapper objectMapper = new ObjectMapper();
+			logger.info(">>>>TmsClient findChargeByOrderIds>FindChargeByOrderIdsRequest url:{}", url);
+			String json = objectMapper.writeValueAsString(request);
+			String response = HttpClientUtil.doPost(url, json);
+			if (!StringUtils.isBlank(response)) {
+				return objectMapper.readValue(response, FindChargeByOrderIdsResponse.class);
+			} else {
+				return FindChargeByOrderIdsResponse.failed("response is blank");
+			}
+		} catch (Exception e) {
+			return FindChargeByOrderIdsResponse.failed(e.getLocalizedMessage());
+			
 		}
 	}
 }
